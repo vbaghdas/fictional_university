3fd435c1dbae4adcfb2046f53fb02150ad2e5066
vbaghdas <vbaghdas@gmail.com>
2018-01-10 01:27:07 -0800

    completed blog post page

--- archive.php
+++ archive.php
@@ -5,10 +5,17 @@ get_header(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-  <h1 class="page-banner__title">Welcome to our blog!</h1>
-  <div class="page-banner__intro">
-      <p>Keep up with our latest news.</p>
-  </div>
+    <h1 class="page-banner__title">
+        <?php if (is_category()) {
+        single_cat_title();
+        }
+        if (is_author()) {
+            echo 'Posts by '; the_author();
+        } ?>
+    </h1>
+    <div class="page-banner__intro">
+        <p><?php the_archive_description(); ?></p>
+    </div>
   </div>  
 </div>
 
--- page.php
+++ page.php
@@ -22,7 +22,7 @@
                 if ($theParent) { ?>
                     <div class="metabox metabox--position-up metabox--with-home-link">
                         <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>">
-                        <i class="fa fa-home" aria-hidden="true"></i><?php echo get_the_title($theParent); ?></a> 
+                        <i class="fa fa-home" aria-hidden="true"></i>Back to<?php echo get_the_title($theParent); ?></a> 
                         <span class="metabox__main"><?php the_title() ?></span></p>
                     </div>
                 <?php }
